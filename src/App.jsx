0200f29a15e99819db9e7c804ee6ef484f482f82
zii <nurazizha41@gmail.com>
2025-05-21 16:20:13 +0700

    gabungan code dengan Havisya dan Fadlan

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import MainLayout from "./assets/components/MainLayout";
 import Dashboard from "./assets/Pages/Dashboard";
-
+import CustomerManagement from "./assets/Pages/CustomerManagement";
+import SalesManagement from "./assets/Pages/SalesManagement";
 function App() {
   return (
     <Routes>
       <Route element={<MainLayout />}>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/Pelanggan" element={<CustomerManagement />} />
+        <Route path="/sales" element={<SalesManagement />} />
       </Route>
     </Routes>
   );
