99c98eb25303a7b19b75ba9fb9a4c1c4dc6a10d2
nrazz41 <nurazizha41@gmail.com>
2025-06-27 22:44:16 +0700

    final kah?

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,6 @@ import NotificationPage from "./assets/Pages/NotificationPage";
 import CartPage from "./assets/Pages/CartPage";
 import PromoPage from "./assets/Pages/PromoPage";
 import CategoryPage from "./assets/Pages/CategoryPage";
-import OrderDetailPage from "./assets/Pages/OrderDetailPage";
 import CheckoutPage from "./assets/pages/CheckoutPage";
 import ChatPage from "./assets/Pages/ChatPage";
 
@@ -54,7 +53,6 @@ function App() {
           <Route path="/cart" element={<CartPage />} />
           <Route path="/promo-page" element={<PromoPage />} />
           <Route path="/category/:categoryName" element={<CategoryPage />} />
-          <Route path="/order/:id" element={<OrderDetailPage />} />
           <Route path="/checkout" element={<CheckoutPage />} />
           <Route path="/chat" element={<ChatPage />} />
           <Route path="/product/:id" element={<ProductDetailPage />} />
